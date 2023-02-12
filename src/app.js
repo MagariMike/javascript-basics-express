32d06b67e8b7ccda46c2348c6dee7da7e4f59892
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-02-12 17:57:35 +0000

    uppercased string test passed

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const app = express();
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: `Hello, ${req.params.string}!` });
 });
+
+app.get('/strings/upper/hello', (req, res) => {
+  res.status(200).send({ result: 'HELLO' });
+});
+
 module.exports = app;
