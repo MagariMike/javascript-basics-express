4b6779a8af59411308795d9d8e3ffcdfd8c9cb90
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-02-12 18:05:01 +0000

    lowercased string test passed

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ app.get('/strings/upper/hello', (req, res) => {
   res.status(200).send({ result: 'HELLO' });
 });
 
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.status(200).send({ result: 'hello' });
+});
+
 module.exports = app;
