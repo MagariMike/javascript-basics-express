f009a60f0515916fc960b9efbbbda322a8a5846a
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-02-13 19:55:20 +0000

    first-characters test passing

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: `Hello, ${req.params.string}!` });
 });
 
-app.get('/strings/upper/hello', (req, res) => {
-  res.status(200).send({ result: 'HELLO' });
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).send({ result: `${req.params.string}`.toUpperCase() });
 });
 
-app.get('/strings/lower/HELLO', (req, res) => {
-  res.status(200).send({ result: 'hello' });
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).send({ result: `${req.params.string}`.toLowerCase() });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.status(200).send({ result: `${req.params.string[0]}` });
 });
 
 module.exports = app;
