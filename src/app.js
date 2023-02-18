44143aec63b27aded14a176189c831caff67d69a
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-02-18 14:42:49 +0000

    first multiply test passing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const { multiply } = require('./lib/numbers');
 const { firstCharacters } = require('./lib/strings');
 
 const app = express();
 
+app.use(express.json());
+
 // STRINGS
 
-/* app.get('/strings/hello/:string', (req, res) => {
+app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: `Hello, ${req.params.string}!` });
 });
 
@@ -17,21 +20,19 @@ app.get('/strings/lower/:string', (req, res) => {
   res.status(200).send({ result: `${req.params.string}`.toLowerCase() });
 });
 
-app.get('/strings/first-characters/:string', (req, res) => {
+/* app.get('/strings/first-characters/:string', (req, res) => {
   res.status(200).send({ result: `${req.params.string[0]}` });
-});
+}); */
 
 app.get('/strings/first-characters/:string', (req, res) => {
-  console.log(req.query);
   res.status(200).send({ result: firstCharacters(req.params.string, req.query.length) });
-}); */
+});
 
 // NUMBERS
 
 app.get('/numbers/add/:a/and/:b', (req, res) => {
   const a = parseInt(req.params.a);
   const b = parseInt(req.params.b);
-  console.log(typeof a, a);
   if (!a && !b) {
     res.status(400).send({ error: 'Parameters must be valid numbers.' });
   } else {
@@ -49,4 +50,11 @@ app.get('/numbers/subtract/:a/from/:b', (req, res) => {
     res.status(200).send({ result: b - a });
   }
 });
+// THE TWO TESTS ABOVE COULD BE ACHIEVED BY CALLING ON NUMBERS METHODS?????
+// LOOK INTO THIS!
+
+app.post('/numbers/multiply', (req, res) => {
+  res.status(200).send({ result: multiply(req.body.a, req.body.b) });
+});
+
 module.exports = app;
--- src/lib/numbers.js
+++ src/lib/numbers.js
@@ -1,51 +1,49 @@
-function add (a, b) {
-return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-function divide (a, b) {
- return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
-function power (a, b) {
-  return Math.pow (a,b);
+function power(a, b) {
+  return Math.pow(a, b);
 }
 
-function round (a) {
-  return Math.round (a);
+function round(a) {
+  return Math.round(a);
 }
 
-function roundUp (a) {
+function roundUp(a) {
   return Math.ceil(a);
 }
 
-function roundDown (a) {
+function roundDown(a) {
   return Math.floor(a);
 }
 
-function absolute (a) {
+function absolute(a) {
   return Math.abs(a);
 }
 
-function quotient (a, b) {
-  let var1 = a / b;
-  if (var1 >= 0){
+function quotient(a, b) {
+  const var1 = a / b;
+  if (var1 >= 0) {
     return Math.floor(var1);
-} else {
+  }
   return Math.ceil(var1);
 }
-  
-}
 
-function remainder (a, b) {
-return a % b; 
+function remainder(a, b) {
+  return a % b;
 }
 
 module.exports = {
@@ -59,5 +57,5 @@ module.exports = {
   roundDown,
   absolute,
   quotient,
-  remainder
-}
+  remainder,
+};
